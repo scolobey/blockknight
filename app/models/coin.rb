53f527b7d1d6111009874d52347f5510d24bae3f
scolobey <ryan-goodwin@cookpad.com>
2017-07-22 16:06:44 -0700

    red and green changes

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,2 +1,12 @@
 class Coin < ApplicationRecord
+  def increased?
+    puts percent_change.class, percent_change
+    if percent_change && percent_change > 0
+      puts 'true'
+      return true
+    else
+      puts 'false'
+      return false
+    end
+  end
 end
