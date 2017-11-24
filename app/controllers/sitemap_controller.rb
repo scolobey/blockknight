214b9b155b3d057ecbc0e98f63652f5fb088e242
scolobey <ryan-goodwin@cookpad.com>
2017-11-24 08:21:05 -0800

    id issue for sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,7 +2,7 @@ class SitemapController < ApplicationController
 
   def index
     # @coins = Coin.all
-    @coins = Coin.find_all_by_id([17305982, 17305992, 17306282, 17306352, 17316292])
+    @coins = Coin.where(id: [17305982, 17305992, 17306282, 17306352, 17316292])
 
     respond_to do |format|
       format.xml
