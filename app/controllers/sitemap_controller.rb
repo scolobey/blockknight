a21983c95ee743790450de766ef3f61d9974f53b
scolobey <ryan-goodwin@cookpad.com>
2017-11-24 08:16:52 -0800

    limited sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,7 +1,8 @@
 class SitemapController < ApplicationController
 
   def index
-    @coins = Coin.all
+    # @coins = Coin.all
+    @coins = Coin.find_all_by_id([17305982, 17305992, 17306282, 17306352, 17316292])
 
     respond_to do |format|
       format.xml
